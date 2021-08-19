2a15ada4a8a74f3fb7a14d96f6397844a2fc6cd3
starzyn <starzyn@qq.com>
2021-08-19 21:27:00 +0800

    feat(core): 网关实现认证调通

--- mall-auth/src/main/java/com/codezzz/mallauth/filter/AuthenticateFilter.java
+++ mall-auth/src/main/java/com/codezzz/mallauth/filter/AuthenticateFilter.java
@@ -78,6 +78,8 @@ public class AuthenticateFilter implements GlobalFilter, Ordered {
                 e.printStackTrace();
             }
         }
+
+
         return chain.filter(exchange);
     }
 
