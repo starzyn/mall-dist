7309d8c3b21e6c7dea896c28df0f179c1fe56518
zhangyn <zhangyn@isyscore.com>
2021-08-19 09:52:30 +0800

    feat(core): mall-dist 网关实现

---
+++ mall-auth/src/main/java/com/codezzz/mallauth/MallAuthApplication.java
@@ -0,0 +1,13 @@
+package com.codezzz.mallauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAuthApplication.class, args);
+    }
+
+}
